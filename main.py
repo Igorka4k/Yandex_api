ed4d88836583f6d726de1dc20efbf8af7ae58deb
syvlline <dovase71@gmail.com>
2021-03-21 18:26:24 +0500

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)  # Загружаем дизайн
+        self.initUI()
+
+    def initUI(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
